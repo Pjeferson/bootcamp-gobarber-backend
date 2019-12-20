5c79eca486e098b637336e0af207346ab5bdb41d
Pjeferson <paulojeferson34@gmail.com>
2019-12-20 15:14:14 -0300

    adding provider schedule

---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,39 @@
+import { parseISO, startOfDay, endOfDay } from 'date-fns';
+import { Op } from 'sequelize';
+
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+import File from '../models/File';
+
+class ScheduleController {
+  async index(req, res) {
+    const checkUserProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkUserProvider) {
+      return res.status(401).json({ error: 'User is not a provider.' });
+    }
+
+    const { date } = req.query;
+    const parsedDate = parseISO(date);
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
+        },
+      },
+      order: ['date'],
+    });
+    return res.send(appointments);
+  }
+  async show(req, res) {}
+  async store(req, res) {}
+  async update(req, res) {}
+  async delete(req, res) {}
+}
+
+export default new ScheduleController();
