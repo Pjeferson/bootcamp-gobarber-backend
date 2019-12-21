b061ce9956e81475c04adb7aac644bbf54a51c8f
Pjeferson <paulojeferson34@gmail.com>
2019-12-21 12:16:12 -0300

    installing nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '4f925a009604c9',
+    pass: '6ff71cb113b85d',
+  },
+  deafult: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
