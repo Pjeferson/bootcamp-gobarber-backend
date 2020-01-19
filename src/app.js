389d09006008c15fca9d978ad13b05cc29f2c43a
Pjeferson <paulojeferson34@gmail.com>
2020-01-19 12:03:20 -0300

    adding cors

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import * as Sentry from '@sentry/node';
 import 'express-async-errors';
 import routes from './routes';
 import sentryConfig from './config/sentry';
+import cors from 'cors';
 
 import './database';
 
@@ -22,6 +23,7 @@ class App {
   }
 
   middlewares() {
+    this.server.use(cors());
     this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
